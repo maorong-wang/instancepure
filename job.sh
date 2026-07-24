762829445101ca90bbc0f1885f6539bf05951d41
maorong <maoro.wang@gmail.com>
2026-07-24 11:12:13 +0900

    before rebuttal

--- job.sh
+++ job.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 #$ -cwd
 #$ -l gpu_1=1
-#$ -l h_rt=16:00:00
+#$ -l h_rt=24:00:00
 #$ -N gpu-job
-#$ -j y -o gpu-job.out
+#$ -j y -o gpu-job3.out
 module purge
 module load cuda
 source ~/.bashrc
-conda activate diffpure
-wandb agent maorong-wang/robustbench-ranpac-hira-rc-sweep_depth/myd1d161
+conda activate robustbench
+
+wandb agent maorong-wang/robustbench-ranpac-hira-apgdcw/do673tgq
